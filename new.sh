c39a8e8fa85b091ef883e02b8fb98a880bc17a01
JoostK <joost.koehoorn@gmail.com>
2022-12-02 23:15:15 +0100

    Project template

---
+++ new.sh
@@ -0,0 +1,16 @@
+DAY=$1
+if [ -z "$DAY" ]; then
+  echo "Missing day argument"
+  exit 1
+fi
+
+PROJECT="day$DAY"
+if [ -d "$PROJECT" ]; then
+  echo "Day $DAY already exists"
+  exit 1
+fi
+
+cp -R template/ "$PROJECT"
+sed -i '' "s/template/$PROJECT/" "$PROJECT/Cargo.toml"
+sed -i '' "s/\# NEW/\"$PROJECT\",\n    # NEW/g" Cargo.toml
+cargo build
