f1ad0400742fcf8cf59931cca5ee769adf429a24
JoostK <joost.koehoorn@gmail.com>
2022-12-17 12:42:58 +0100

    Day 16 template (skipped)

---
+++ day16/src/part1.rs
@@ -0,0 +1,16 @@
+pub fn run(_input: &str) -> usize {
+    // input.lines().count()
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn examples() {
+        let example = "";
+
+        assert_eq!(run(example), 0);
+    }
+}
