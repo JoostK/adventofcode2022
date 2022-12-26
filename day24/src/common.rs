c2c9cb1e7f8c05ccde8bd92cf5d17d2d6447e2d8
JoostK <joost.koehoorn@gmail.com>
2022-12-26 14:26:41 +0100

    Day 24 even faster

--- day24/src/common.rs
+++ day24/src/common.rs
@@ -1,36 +1,32 @@
-use shared::direction::Direction;
+use std::collections::VecDeque;
 use std::fmt::{Display, Formatter, Write};
 
-#[derive(Clone)]
-pub struct Blizzard {
-    pub direction: Direction,
-}
-
-#[derive(Clone)]
 pub struct Grid {
     pub width: usize,
     pub height: usize,
-    pub blizzards: Vec<Vec<Blizzard>>,
+    pub left: VecDeque<Vec<bool>>,
+    pub right: VecDeque<Vec<bool>>,
+    pub up: VecDeque<Vec<bool>>,
+    pub down: VecDeque<Vec<bool>>,
 }
 
 impl Display for Grid {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        for y in 0..self.height {
-            let mut c = y * self.width;
-            for _ in 0..self.width {
-                let blizzards = &self.blizzards[c];
-                let m = match blizzards.len() {
+        for y in 0..(self.height - 2) {
+            for x in 0..self.width {
+                let left = self.left[x][y];
+                let right = self.right[x][y];
+                let up = self.up[y][x];
+                let down = self.down[y][x];
+                let m = match left as u8 + right as u8 + up as u8 + down as u8 {
                     0 => '.',
-                    1 => match blizzards.first().unwrap().direction {
-                        Direction::Up => '^',
-                        Direction::Down => 'v',
-                        Direction::Left => '<',
-                        Direction::Right => '>',
-                    },
+                    1 if left => '<',
+                    1 if right => '>',
+                    1 if up => '^',
+                    1 => 'v',
                     n => (n as u8 + b'0') as char,
                 };
                 f.write_char(m)?;
-                c += 1;
             }
             writeln!(f)?;
         }
@@ -47,84 +43,63 @@ impl From<&str> for Grid {
             height += 1;
         }
 
-        let mut blizzards = vec![Vec::new(); width * height];
+        let mut left = VecDeque::with_capacity(width);
+        let mut right = VecDeque::with_capacity(width);
+        for _ in 0..width {
+            left.push_back(vec![false; height - 2]);
+            right.push_back(vec![false; height - 2]);
+        }
+
+        let mut up = VecDeque::with_capacity(height - 2);
+        let mut down = VecDeque::with_capacity(height - 2);
+        for _ in 0..(height - 2) {
+            up.push_back(vec![false; width]);
+            down.push_back(vec![false; width]);
+        }
 
         for (y, line) in input.lines().skip(1).enumerate() {
-            let mut c = (y + 1) * width;
-            for b in line[1..line.len()].bytes() {
+            for (x, b) in line[1..line.len()].bytes().enumerate() {
                 match b {
                     b'.' => (),
-                    b'v' => blizzards[c].push(Blizzard {
-                        direction: Direction::Down,
-                    }),
-                    b'^' => blizzards[c].push(Blizzard {
-                        direction: Direction::Up,
-                    }),
-                    b'<' => blizzards[c].push(Blizzard {
-                        direction: Direction::Left,
-                    }),
-                    b'>' => blizzards[c].push(Blizzard {
-                        direction: Direction::Right,
-                    }),
+                    b'<' => left[x][y] = true,
+                    b'>' => right[x][y] = true,
+                    b'v' => down[y][x] = true,
+                    b'^' => up[y][x] = true,
                     b'#' => break,
                     _ => panic!("unknown symbol"),
                 }
-                c += 1;
             }
         }
 
         Self {
             width,
             height,
-            blizzards,
+            left,
+            right,
+            up,
+            down,
         }
     }
 }
 
 impl Grid {
-    fn simulate(&self, into: &mut Grid) {
-        for b in &mut into.blizzards {
-            b.clear();
-        }
-        for (c, blizzards) in self.blizzards.iter().enumerate() {
-            for b in blizzards {
-                let new_c = match b.direction {
-                    Direction::Left => {
-                        if c % self.width == 0 {
-                            c + self.width - 1
-                        } else {
-                            c - 1
-                        }
-                    }
-                    Direction::Right => {
-                        if c % self.width == self.width - 1 {
-                            c + 1 - self.width
-                        } else {
-                            c + 1
-                        }
-                    }
-                    Direction::Up => {
-                        if c / self.width == 1 {
-                            c + (self.width * (self.height - 2)) - self.width
-                        } else {
-                            c - self.width
-                        }
-                    }
-                    Direction::Down => {
-                        if c / self.width == self.height - 2 {
-                            c + self.width - (self.width * (self.height - 2))
-                        } else {
-                            c + self.width
-                        }
-                    }
-                };
-                into.blizzards[new_c].push(b.clone());
-            }
-        }
+    fn simulate(&mut self) {
+        self.left.rotate_left(1);
+        self.right.rotate_right(1);
+        self.up.rotate_left(1);
+        self.down.rotate_right(1);
     }
 
     fn is_free(&self, c: usize) -> bool {
-        self.blizzards[c].is_empty()
+        let x = c % self.width;
+        let y = c / self.width;
+        if y == 0 {
+            return x == 0;
+        } else if y == self.height - 1 {
+            return x == self.width - 1;
+        }
+        let y = y - 1;
+        !self.left[x][y] && !self.right[x][y] && !self.up[y][x] && !self.down[y][x]
     }
 }
 
@@ -142,33 +117,32 @@ pub fn walk(start: usize, goal: usize, mut grid: Grid) -> (usize, Grid) {
 
     pending.push(start);
 
-    let mut next_grid = grid.clone();
     while !marked[goal] {
-        grid.simulate(&mut next_grid);
+        grid.simulate();
 
         for &c in &pending {
             marked[c] = false;
             let x = c % width;
             let y = c / width;
 
-            if y != height - 1 && x > 0 && !next_marked[c - 1] && next_grid.is_free(c - 1) {
+            if y != height - 1 && x > 0 && !next_marked[c - 1] && grid.is_free(c - 1) {
                 next_marked[c - 1] = true;
                 next_pending.push(c - 1);
             }
-            if y != 0 && x < width - 1 && !next_marked[c + 1] && next_grid.is_free(c + 1) {
+            if y != 0 && x < width - 1 && !next_marked[c + 1] && grid.is_free(c + 1) {
                 next_marked[c + 1] = true;
                 next_pending.push(c + 1);
             }
-            if y > 1 && !next_marked[c - width] && next_grid.is_free(c - width) {
+            if y > 1 && !next_marked[c - width] && grid.is_free(c - width) {
                 next_marked[c - width] = true;
                 next_pending.push(c - width);
             }
-            if y < height - 2 && !next_marked[c + width] && next_grid.is_free(c + width) {
+            if y < height - 2 && !next_marked[c + width] && grid.is_free(c + width) {
                 next_marked[c + width] = true;
                 next_pending.push(c + width);
             }
 
-            if !next_marked[c] && next_grid.is_free(c) {
+            if !next_marked[c] && grid.is_free(c) {
                 next_marked[c] = true;
                 next_pending.push(c);
             }
@@ -176,7 +150,6 @@ pub fn walk(start: usize, goal: usize, mut grid: Grid) -> (usize, Grid) {
 
         pending.clear();
 
-        std::mem::swap(&mut grid, &mut next_grid);
         std::mem::swap(&mut marked, &mut next_marked);
         std::mem::swap(&mut pending, &mut next_pending);
         depth += 1;
